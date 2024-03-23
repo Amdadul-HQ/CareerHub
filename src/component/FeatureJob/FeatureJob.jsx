32bfca371714bf2c38f3bcf661f74a27da4245d2
AmdadulHQ <rimonamdadul301@gmail.com>
2024-03-23 12:50:06 +0600

    adding feature jobs section

--- src/component/FeatureJob/FeatureJob.jsx
+++ src/component/FeatureJob/FeatureJob.jsx
@@ -3,20 +3,21 @@ import { AiOutlineDollarCircle } from "react-icons/ai";
 const FeatureJob = ({job}) => {
     const {logo,job_title,company_name,remote_or_onsite,location,job_type,salary} = job
     return (
-        <div>
+        <div className="border p-10 rounded-lg">
             <div>
                 <img src={logo} alt="" />
             </div>
-            <h1>{job_title}</h1>
-            <p>{company_name}</p>
-            <div>
-            <button>{remote_or_onsite}</button>
-            <button>{job_type}</button>
+            <h1 className="text-[#474747] text-2xl font-bold mt-8">{job_title}</h1>
+            <p className="text-[#757575] text-xl font-semibold mt-2">{company_name}</p>
+            <div className="text-[#9873FF] text-base font-bold flex gap-x-4 mt-4">
+            <button className="px-5 py-2 rounded border border-[#7E90FE]">{remote_or_onsite}</button>
+            <button className="px-5 py-2 rounded border border-[#7E90FE]">{job_type}</button>
             </div>
-            <div>
-                <p><CiLocationOn></CiLocationOn> {location}</p>
-                <p><AiOutlineDollarCircle></AiOutlineDollarCircle>{salary}</p>
+            <div className="text-[#757575] text-xl font-medium flex gap-x-6 mt-4">
+                <p className="flex items-center"><CiLocationOn className="text-2xl"></CiLocationOn> {location}</p>
+                <p className="flex items-center"><AiOutlineDollarCircle className="text-2xl"></AiOutlineDollarCircle>{salary}</p>
             </div>
+            <button className="text-white text-xl font-bold px-7 py-4 rounded-lg bg-gradient-to-r from-[#7E90FE] to-[#9873FF] mt-6">View Details</button>
         </div>
     );
 };
--- src/component/Featurejobs/FeatureJobs.jsx
+++ src/component/Featurejobs/FeatureJobs.jsx
@@ -15,7 +15,6 @@ const FeatureJobs = () => {
             <Pageheading title='Featured Jobs'
              discription='Explore thousands of job opportunities with all the information you need. Its your future'>
              </Pageheading>
-             <h1>total JOb : {jobs.length}</h1>
              <div className="grid  grid-cols-2 gap-6">
              {
                 jobs.map(job => <FeatureJob key={job.id} job={job} ></FeatureJob>)
